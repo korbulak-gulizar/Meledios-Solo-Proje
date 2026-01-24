a0f4c7ded0d82b9aaeec09b165f768211cd6f6e2
Gulizar Korbulak <korbulakgulizar@gmail.com>
2026-01-24 21:07:32 +0300

    Buttonlar Yapildi, Efectler verildi, Hepsi Homepage sayfasinda denendi.

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,7 +1,27 @@
+import Button from "../../Components/Buttons/Button";
+import {
+  FacebookIcon,
+  GoogleIcon,
+} from "../../Components/Buttons/iconsButtons";
 const Home = () => {
   return (
     <div>
       <h1>Welcome to the Home Page</h1>
+      <header>
+        <div style={{ display: "flex", gap: "20px" }}>
+          <Button variant="secondary" label="Login" />
+          <Button variant="primary" label="Sign Up" />
+          <Button variant="createPlayList" label="Create Playlist" />
+          <Button variant="discoverNow" label="Discover Now" />
+
+          <Button variant="google" icon={<GoogleIcon />} label="Google Login" />
+          <Button
+            variant="facebook"
+            icon={<FacebookIcon />}
+            label="FAcebook Login"
+          />
+        </div>
+      </header>
     </div>
   );
 };
