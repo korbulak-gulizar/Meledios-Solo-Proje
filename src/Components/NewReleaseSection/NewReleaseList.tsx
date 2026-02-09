e7ed24a7c2c2d1bbdbc831fbc546fb812821cd66
Gulizar Korbulak <korbulakgulizar@gmail.com>
2026-02-09 22:39:39 +0100

    newReleaseSection yapildi

---
+++ src/Components/NewReleaseSection/NewReleaseList.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import type { VerticalCardProps } from "../Cards/VerticalCard";
+import VerticalCards from "../Cards/VerticalCard";
+import { cards } from "./newRelCardsData";
+import styles from "./../NewReleaseSection/styleList.module.scss";
+import { ViewAll } from "../WievAllIcon/WievAllicon";
+
+const CardList: React.FC = () => {
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.header}>
+        <h1 className={styles.titleWeekly}>
+          Weekly Top <span>Songs</span>
+        </h1>
+      </div>
+
+      <div className={styles.cardList}>
+        {cards.map((card: VerticalCardProps, index: number) => (
+          <VerticalCards key={index} {...card} />
+        ))}
+        <ViewAll />
+      </div>
+    </div>
+  );
+};
+
+export default CardList;
