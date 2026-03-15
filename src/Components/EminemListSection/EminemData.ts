ff15d7b591dac310ff6a0bfe7ea276f835f55d57
Gulizar Korbulak <korbulakgulizar@gmail.com>
2026-03-16 00:25:23 +0100

    Eminem Listesi yapildi

---
+++ src/Components/EminemListSection/EminemData.ts
@@ -0,0 +1,15 @@
+import type { RoundCardProps } from "./../Cards/RoundCard";
+import cent from "../EminemListSection/images/cent.png";
+import snoop from "../EminemListSection/images/snoop.png";
+import tupac from "../EminemListSection/images/tupac.png";
+import jay from "../EminemListSection/images/jay.png";
+
+export const cards: RoundCardProps[] = [
+  {
+    description: "50 Cent",
+    imageUrl: cent,
+  },
+  { description: "Snoop Dog", imageUrl: snoop },
+  { description: "Tupac", imageUrl: tupac },
+  { description: "Jay+Z", imageUrl: jay },
+];
